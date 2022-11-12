a3987da5e277c8fdd38556e6fa0fe0e8b737d62f
NguyenAnhDuc <anhducnguyen0703@gmail.com>
2022-11-12 13:53:45 +0700

    TEST 1 -T1

---
+++ WebApplication1/WebApplication1/Models/EF/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models.EF
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Code { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
